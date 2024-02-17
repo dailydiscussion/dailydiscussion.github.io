00d90ac7249340c1fa57bf105fe47c9f050135fc
dailydiscussion <drnikhilkr16@gmail.com>
2024-02-17 19:47:02 +0530

    1

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging.js');
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVbqSosUmhwpnoXAg5zL9CAC07fN5AL8Q",
