4bfca8fa3eece384a2c19e3149092794dd7b0127
dailydiscussion <drnikhilkr16@gmail.com>
2024-02-23 15:26:48 +0530

    day41

--- script.js
+++ script.js
@@ -166,107 +166,115 @@ setInterval(updateDate, 1000);
 // js for event calendar //
 
 document.addEventListener("DOMContentLoaded", function() {
-const currentDate = new Date();
-let currentMonth = currentDate.getMonth() + 1; // Adjust for zero-based month
-let currentYear = currentDate.getFullYear();
-let todayDate = currentDate.getDate(); // Get today's date
+    const currentDate = new Date();
+    let currentMonth = currentDate.getMonth() + 1; // Adjust for zero-based month
+    let currentYear = currentDate.getFullYear();
+    let todayDate = currentDate.getDate(); // Get today's date
 
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
-const currentMonthText = document.getElementById("currentMonth");
-const daysContainer = document.getElementById("daysContainer");
-const eventContainer = document.getElementById("eventContainer");
+    const prevBtn = document.getElementById("prevBtn");
+    const nextBtn = document.getElementById("nextBtn");
+    const currentMonthText = document.getElementById("currentMonth");
+    const daysContainer = document.getElementById("daysContainer");
+    const eventContainer = document.getElementById("eventContainer");
 
-prevBtn.addEventListener("click", function() {
-updateCalendar(-1);
-});
+    prevBtn.addEventListener("click", function() {
+        updateCalendar(-1);
+    });
 
-nextBtn.addEventListener("click", function() {
-updateCalendar(1);
-});
+    nextBtn.addEventListener("click", function() {
+        updateCalendar(1);
+    });
 
-function updateCalendar(delta) {
-currentMonth += delta;
-if (currentMonth < 1) {
-currentMonth = 12;
-currentYear--;
-} else if (currentMonth > 12) {
-currentMonth = 1;
-currentYear++;
-}
-renderCalendar();
-}
+    function updateCalendar(delta) {
+        currentMonth += delta;
+        if (currentMonth < 1) {
+            currentMonth = 12;
+            currentYear--;
+        } else if (currentMonth > 12) {
+            currentMonth = 1;
+            currentYear++;
+        }
+        renderCalendar();
+    }
 
-function renderCalendar() {
-const monthNames = ["January", "February", "March", "April", "May", "June",
-"July", "August", "September", "October", "November", "December"
-];
-const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    function renderCalendar() {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        const dayNames = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
 
-currentMonthText.textContent = monthNames[currentMonth - 1] + ' ' + currentYear;
-const fragment = document.createDocumentFragment(); // Create a document fragment
+        currentMonthText.textContent = monthNames[currentMonth - 1] + ' ' + currentYear;
+        const fragment = document.createDocumentFragment(); // Create a document fragment
 
-fetch('calendar-event.json')
-.then(response => {
-if (!response.ok) {
-throw new Error('Network response was not ok');
-}
-return response.json();
-})
-.then(data => {
-const events = data;
+        fetch('calendar-event.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const events = data;
 
-const firstDayOfMonth = new Date(currentYear, currentMonth - 1, 1);
-const startDayIndex = firstDayOfMonth.getDay();
+                const firstDayOfMonth = new Date(currentYear, currentMonth - 1, 1);
+                const startDayIndex = firstDayOfMonth.getDay();
 
-const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+                const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
 
-for (let i = 0; i < startDayIndex; i++) {
-const emptyDayElement = document.createElement('div');
-emptyDayElement.classList.add('empty-day');
-fragment.appendChild(emptyDayElement); // Append empty day to fragment
-}
+                // Append day names
+                for (let i = 0; i < 7; i++) {
+                    const dayNameElement = document.createElement('div');
+                    dayNameElement.classList.add('day-name');
+                    dayNameElement.textContent = dayNames[i];
+                    fragment.appendChild(dayNameElement); // Append day name to fragment
+                }
 
-for (let i = 1; i <= daysInMonth; i++) {
-const dayElement = document.createElement('div');
-dayElement.classList.add('day-app');
-dayElement.textContent = i;
+                for (let i = 0; i < startDayIndex; i++) {
+                    const emptyDayElement = document.createElement('div');
+                    emptyDayElement.classList.add('empty-day');
+                    fragment.appendChild(emptyDayElement); // Append empty day to fragment
+                }
 
-if (i === todayDate && currentMonth === currentDate.getMonth() + 1 && currentYear === currentDate.getFullYear()) {
-dayElement.classList.add('today');
-}
+                for (let i = 1; i <= daysInMonth; i++) {
+                    const dayElement = document.createElement('div');
+                    dayElement.classList.add('day-app');
+                    dayElement.textContent = i;
 
-dayElement.addEventListener("click", function() {
-const paddedMonth = currentMonth.toString().padStart(2, '0');
-const dateKey = `${currentYear}-${paddedMonth}-${i}`;
-const event = events[dateKey];
-if (event) {
-eventContainer.querySelector('.date-day').textContent = dayNames[new Date(dateKey).getDay()];
-eventContainer.querySelector('.date').textContent = i;
-eventContainer.querySelector('.event').innerHTML = event.event;
-} else {
-eventContainer.querySelector('.date-day').textContent = dayNames[new Date(dateKey).getDay()];
-eventContainer.querySelector('.date').textContent = i;
-eventContainer.querySelector('.event').innerHTML = "<p>No event found for this date</p>";
-}
-});
+                    if (i === todayDate && currentMonth === currentDate.getMonth() + 1 && currentYear === currentDate.getFullYear()) {
+                        dayElement.classList.add('today');
+                    }
 
-fragment.appendChild(dayElement); // Append day element to fragment
+                    dayElement.addEventListener("click", function() {
+                        const paddedMonth = currentMonth.toString().padStart(2, '0');
+                        const dateKey = `${currentYear}-${paddedMonth}-${i}`;
+                        const event = events[dateKey];
+                        if (event) {
+                            eventContainer.querySelector('.date-day').textContent = dayNames[new Date(dateKey).getDay()];
+                            eventContainer.querySelector('.date').textContent = i;
+                            eventContainer.querySelector('.event').innerHTML = event.event;
+                        } else {
+                            eventContainer.querySelector('.date-day').textContent = dayNames[new Date(dateKey).getDay()];
+                            eventContainer.querySelector('.date').textContent = i;
+                            eventContainer.querySelector('.event').innerHTML = "<p>No event found for this date</p>";
+                        }
+                    });
 
-if (i === todayDate && currentMonth === currentDate.getMonth() + 1 && currentYear === currentDate.getFullYear()) {
-dayElement.click(); // Trigger click event for today's date
-}
-}
+                    fragment.appendChild(dayElement); // Append day element to fragment
 
-daysContainer.innerHTML = ''; // Clear the container
-daysContainer.appendChild(fragment); // Append fragment to container
-})
-.catch(error => {
-console.error('Error fetching event data:', error);
-});
-}
+                    if (i === todayDate && currentMonth === currentDate.getMonth() + 1 && currentYear === currentDate.getFullYear()) {
+                        dayElement.click(); // Trigger click event for today's date
+                    }
+                }
 
-renderCalendar();
+                daysContainer.innerHTML = ''; // Clear the container
+                daysContainer.appendChild(fragment); // Append fragment to container
+            })
+            .catch(error => {
+                console.error('Error fetching event data:', error);
+            });
+    }
+
+    renderCalendar();
 });
 
 
